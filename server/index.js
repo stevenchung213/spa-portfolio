e469fc9d24082a4c71daff6191f3be51e9bf42f7
Steven Chung <stevenchung.213@gmail.com>
2019-03-22 16:32:32 -0700

    cleaned up unnecessary code. strictly relying on @media queries for wallpaper

--- server/index.js
+++ server/index.js
@@ -1,21 +1,30 @@
-import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Main from '../src/components/Main.jsx';
-import Html from '../src/html.jsx';
+const express = require('express');
+var fs = require('fs');
+var https = require('https');
+const path = require('path');
+const compression = require('compression');
 
 const port = 10000;
 const app = express();
 
+app.use(compression());
 
-//server-side render route
+/* DISABLED: CONFLICTS WITH SERVICE WORKER */
+// app.get('*.js', (req, res, next) => {
+//   console.log(req.url);
+//   req.url = req.url + '.gz';
+//   res.set('Content-Encoding', 'gzip');
+//   res.set('Content-Type', 'text/javascript');
+//   next();
+// });
+
+app.use(express.static(path.resolve(__dirname + '/../dist')));
+
+
 app.get('/', (req, res) => {
-
-  const main = renderToString(<Main/>);
-  const title = "Hello~";
-
-  res.send(Html(main, title));
+  res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
+
 
 
 app.listen(port, () => {
